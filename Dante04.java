ac3f9f70e410bf56bcaaaa354fb8e28994ff7b61
Dante9527 <s24066905@gmail.com>
2016-08-16 20:12:23 +0800

    Update Dante04.java

--- Dante04.java
+++ Dante04.java
@@ -1,12 +1,20 @@
 package tw.org.iii;
 
+import java.util.*;
+
 public class Dante04 {
 
 	public static void main(String[] args) 
 	{
-		double year = (int)(Math.random() * 500 + 1) ;
+		// double year = (int)(Math.random() * 500 + 1) ;
 		
-		System.out.println("西元 " + (int)year +" 年") ;
+		// System.out.println("西元 " + (int)year +" 年") ;
+		
+		System.out.println("請輸入西元年分");
+   	        Scanner scan=new Scanner(System.in);
+   	        String Input;
+   	        Input=scan.nextLine();
+   	        int year=Integer.parseInt(Input);
 		
 		if(year %400 == 0)
 		{
