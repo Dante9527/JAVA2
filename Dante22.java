0aff93d720a4e968484495018af30dc96b460bf4
Dante9527 <s24066905@gmail.com>
2016-08-24 18:14:36 +0800

    Update Dante22.java

--- Dante22.java
+++ Dante22.java
@@ -6,79 +6,65 @@ import java.io.IOException;
 
 public class Dante22
 {
+    String[] capital = {" ","拾","佰","仟","萬","拾萬","佰萬","仟萬","億","拾億","佰億","仟億"} ;
+    String[] word={" ","壹","貳","叁","肆","伍","陸","柒","捌","玖"} ;
+    String result = "" ;
+    String qs ;
 	
-	String[] capital = {" ","拾","佰","仟","萬","拾萬","佰萬","仟萬","億","拾億","佰億","仟億"} ;
-	String[] word={" ","壹","貳","叁","肆","伍","陸","柒","捌","玖"} ;
-	public String qs ;
-	public String result = "" ;
 
-	public Dante22(String qs)
-	{
-		this.qs = qs ;
-	}
+    public Dante22(String qs)
+    {
+        this.qs = qs ;
+    }
 	
-	public static void main(String[] args) 
-	{
-		String r = "0" ;
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)) ;
-		
-		do
-		{
-			System.out.println("請輸入正整數 : ");
-			try
-			{
-				r = in.readLine() ;
-				r = Integer.parseInt(r) + "";
-				
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		while(!(Integer.parseInt(r) > 0 && Integer.parseInt(r) < 100000000)) ;
-		
-		Dante22 cn = new Dante22(r + "") ;
-		System.out.println(cn.getUpper() + " 元正") ;
-	}
-	
-	public String getUpper()
-	{
-		com() ;
-		return result ;
-	}
-	
-	public void com()
-	{
-		turnZero() ;
-		turnNumNotZero() ;
-	}
-	
-	 private void turnZero() 
-	 {
-		  if(result.lastIndexOf("0") == result.length() -1)
-		  {
-			  result = result.substring(0,result.length()-1) ;
-		  }
-		  
-		  result = result.replaceAll("0",word[0]) ;
-	 }
-	 
-	public void turnNumNotZero()
-	{
-		for(int i = 0 ; i < qs.length() ; i++ )
-		{
-			int index = Integer.parseInt(this.qs.charAt(i) + "") ;
+    public static void main(String[] args)
+    {
+    	String r = "0" ;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in)) ;
+	    
+        do
+        {
+            ystem.out.println("請輸入正整數 : ") ;
 			
-			if(index != 0)
-			{
-				result += word[index] + capital[qs.length() -i -1] ;
-			}
-			else
-			{
-				result += "0" ;
-			}
-		}
-	}
+            try //得到輸入正整數 
+	    {
+		r = in.readLine() ; 
+		r = Integer.parseInt(r) + "" ;  
+	    }
+            catch(IOException e)
+	    {
+                e.printStackTrace() ;
+            }
+        }
+        while(!(Integer.parseInt(r) > 0 && Integer.parseInt(r) < 1000000000)) ; 
+	    
+        Dante22 cn = new Dante22(r + "") ;
+        System.out.println(cn.getUpper() + " 元正");
+
+    }
+	
+    public String getUpper()
+    {
+        this.com() ;
+        return this.result ;
+    }
+	
+    public void com()
+    {
+        this.turnNumNotZero() ;
+    }
+	
+    public void turnNumNotZero() //將非0數字轉換為大寫
+    {
+    	for(int i = 0 ; i < this.qs.length() ; i ++)
+    	{
+            int index = Integer.parseInt(this.qs.charAt(i) + "") ;
+            //charAt : 返回指定索引的char值
+    		
+            if(index != 0)
+            {
+                this.result += this.word[index] + this.capital[this.qs.length() -i -1] ;
+            }
+         }
+    }
 }
